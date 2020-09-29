612d44de86f7234eebd6d4a6dc229c6f84debe1c
KuniutkoDaria <38259171+KuniutkoDaria@users.noreply.github.com>
2020-09-29 23:20:34 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    if (Boolean(array) && array.length > 0) {
+        const getMin = (array) => array.reduce((a, b) => Math.min(a, b));
+        return min = getMin(array);
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Boolean(array) && array.length > 0) {
+        const getMax = (array) => array.reduce((a, b) => Math.max(a, b));
+        return max = getMax(array);
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Boolean(array) && array.length > 0) {
+        let result = array.reduce((sum, current) => sum + current, 0);
+        return result / array.length;
+    }
+    return 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+
